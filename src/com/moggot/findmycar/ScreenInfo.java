21d17f03664ae2300503d7bde363917c8b91cd07
moggot <9.tilov@gmail.com>
2015-08-26 20:32:46 +0300

    добавил возможность оставлять отзывы

--- src/com/moggot/findmycar/ScreenInfo.java
+++ src/com/moggot/findmycar/ScreenInfo.java
@@ -24,5 +24,19 @@ public class ScreenInfo extends TrackedActivity {
 		textViewAppName.setTypeface(font);
 		TextView textViewInfo = (TextView) findViewById(R.id.textViewInfo);
 		textViewInfo.setTypeface(font);
+
+		ImageView imageViewReview = (ImageView) findViewById(R.id.ivReview);
+
+		imageViewReview.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.d(LOG_TAG, "imageViewReview");
+
+				Uri uri = Uri
+						.parse("https://play.google.com/store/apps/details?id=com.moggot.findmycar");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(intent);
+			}
+		});
 	}
 }
