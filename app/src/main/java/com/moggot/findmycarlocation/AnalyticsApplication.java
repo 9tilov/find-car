cdc62031e57a44aef8f2644a8077dccfef409728
moggot <9.tilov@gmail.com>
2016-12-05 10:15:36 +0300

    изменил версию библиотек, извился от 65k+ methods

--- app/src/main/java/com/moggot/findmycarlocation/AnalyticsApplication.java
+++ app/src/main/java/com/moggot/findmycarlocation/AnalyticsApplication.java
@@ -16,8 +16,6 @@ package com.moggot.findmycarlocation;
  */
 
 import android.app.Application;
-import android.content.Context;
-import android.support.multidex.MultiDex;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -41,13 +39,6 @@ public class AnalyticsApplication extends Application {
     public AnalyticsApplication() {
         super();
     }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
-
 
     synchronized Tracker getTracker(TrackerName trackerId) {
         if (!mTrackers.containsKey(trackerId)) {
